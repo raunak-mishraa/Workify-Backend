aa82b6f75e15c32bac184325658d035a24e44b7e
raunak mishra <raunakmishra6301@gmail.com>
2024-03-25 23:30:05 +0530

    deployment

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,5 +1,42 @@
 import {Gig} from "../models/gig.model.js";
 import {Order} from "../models/order.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import Stripe from "stripe";
+const intent = asyncHandler(async (req, res) =>{
+    const stripe = new Stripe(process.env.STRIPE);
+    // const paymentMethodDomain = await stripe.paymentMethodDomains.create({
+    //     domain_name: 'raunak.com',
+    //   });
+    const gig = await Gig.findById(req.params.id);
+
+   
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    clientId: req.user._id,
+    freelancerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+})
+
+
+//this below code was for testing purpose only
 const createOrder = async (req, res) => {
     const gig = await Gig.findById(req.params.gigId);
 
@@ -31,4 +68,7 @@ const getOrders = async (req, res) => {
     res.status(200).json(orders);
 };
 
-export { createOrder, getOrders };
+export { 
+    getOrders, 
+    intent
+};
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,10 +1,11 @@
 import e from 'express';
 import express from 'express';
-import { createOrder, getOrders } from '../controllers/order.controller.js';
+import { intent, getOrders } from '../controllers/order.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
-router.route('/:gigId').post(verifyJWT, createOrder)
+// router.route('/:gigId').post(verifyJWT, createOrder)
 router.route('/').get(verifyJWT, getOrders)
+router.route("/create-payment-intent/:id").post(verifyJWT, intent)
 
 export default router;
