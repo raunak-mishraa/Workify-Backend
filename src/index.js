295c547855ec2439ceaa887c09653f4073faa49b
raunak mishra <raunakmishra6301@gmail.com>
2024-03-26 17:46:21 +0530

    testing deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+dotenv.config();
+
 import ConnectDB from "./db/index.js";
 import {app} from "./app.js";
-dotenv.config({
-    path: './.env'
-});
 
+// dotenv.config({
+//     path: './.env'
+// });
+console.log(process.env.MONGODB_URI);
+console.log(process.env.PORT);
 ConnectDB()
 .then(() =>{
     app.listen(process.env.PORT || 8000, () => {
