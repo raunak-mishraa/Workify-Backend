07856f9bb3f11926df7242b37510bfbb02244c82
raunak mishra <raunakmishra6301@gmail.com>
2024-04-03 16:19:08 +0530

    added token expiry fucntionality

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,12 +2,13 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
-export const verifyJWT = asyncHandler(async (req, _, next) => {//yaha pe hum res use nahi kar rahe the so we did _
+export const verifyJWT = asyncHandler(async (req, res, next) => {//yaha pe hum res use nahi kar rahe the so we did _
    try {
      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "") 
  
      if(!token){
-         throw new ApiError(401, "Unauthorized request")
+      res.status(200).json(new ApiError(401, "Unauthorized request"))
+        //  throw new ApiError(401, "Unauthorized request")
      }
      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
      const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
@@ -20,3 +21,27 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {//yaha pe hum res
         throw new ApiError(401, error?.message || "Invalid Access Token")
    }
 })
+
+
+// import { ApiError } from "../utils/ApiError.js";
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import jwt from "jsonwebtoken";
+// import { User } from "../models/user.model.js";
+// export const verifyJWT = asyncHandler(async (req, _, next) => {//yaha pe hum res use nahi kar rahe the so we did _
+//    try {
+//      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "") 
+ 
+//      if(!token){
+//          throw new ApiError(401, "Unauthorized request")
+//      }
+//      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//      const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+//      if(!user){
+//          throw new ApiError(401, "Invalid Access Token")
+//      }
+//      req.user = user;//yaha per hum ek naya object add kar rahe user object
+//      next();
+//    } catch (error) {
+//         throw new ApiError(401, error?.message || "Invalid Access Token")
+//    }
+// })
